5e3344de01e3a725741468932e73fedc9c7e9f7a
supermancbs <clark.sacktor@gmail.com>
2016-09-13 17:54:51 -0400

    added bootstraps

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,14 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-  helper_method :current_user 
+  helper_method :current_user
 
   def authorize
     redirect_to '/login' unless current_user
+  end
+
+  def proper_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
 end
