5e936d744033676a601242127fba456826d713cd
supermancbs <clark.sacktor@gmail.com>
2016-09-13 13:25:12 -0400

    added withdrawl form and creation

--- app/controllers/withdrawls_controller.rb
+++ app/controllers/withdrawls_controller.rb
@@ -1,2 +1,17 @@
 class WithdrawlsController < ApplicationController
+
+  def create
+    @withdrawl = Withdrawl.create(withdrawls_params)
+    @user = User.find(withdrawls_params[:user_id])
+    @user.lower_balance(withdrawls_params[:amount])
+    @user.save
+    redirect_to user_path (@user)
+  end
+
+  private
+
+  def withdrawls_params
+    params.require(:withdrawl).permit(:amount, :user_id)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   has_many :buys, :class_name => 'Transaction', :foreign_key => 'buyer_id'
   has_many :sells, :class_name => 'Transaction', :foreign_key => 'seller_id'
 
+  def lower_balance(amount)
+    self.balance = self.balance.to_i - amount.to_i
+  end
+
 end
