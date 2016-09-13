3693b79411a62b8b833aa80eecda13e4d1da7c17
supermancbs <clark.sacktor@gmail.com>
2016-09-13 11:42:54 -0400

    user passes lodgements test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,9 +19,18 @@ require 'rails_helper'
 
 
 RSpec.describe User, type: :model do
+
   it "has withdrawls" do
     clark = User.create!(name: "Clark", address: "123 Main", phone_number: "917", balance: 50)
     withdrawls = Withdrawl.create!(user: clark, amount: 10)
     expect(clark.withdrawls).to eq([withdrawls])
   end
+
+  it "has lodgements" do
+    clark = User.create!(name: "Clark", address: "123 Main", phone_number: "917", balance: 50)
+    lodgement = Lodgement.create!(user: clark, amount: 10)
+
+    expect(clark.lodgements).to eq([lodgement])
+  end
+
 end
